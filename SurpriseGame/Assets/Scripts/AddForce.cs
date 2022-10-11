a32d68b3959445481848e672e0b4af55c8daefbc
KuroTori <1141759094@qq.com>
2022-10-11 12:28:09 -0400

    Door Control

--- SurpriseGame/Assets/Scripts/AddForce.cs
+++ SurpriseGame/Assets/Scripts/AddForce.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AddForce : MonoBehaviour
 {
     public bool Open=false;
+    private Ray ray;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +15,37 @@ public class AddForce : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("I pushed!");
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 10f))
+            {
+                Debug.Log("I Hit Something! "+ raycastHit.transform.tag);
+                if (raycastHit.transform.CompareTag("Door"))
+                {
+                    Debug.Log("It's Door!");
+                    raycastHit.transform.GetComponent<AddForce>().SetOpen();
+                }
+                
+            }
+        }
         OpenDoor();
     }
 
     public void OpenDoor()
     {
-        if (Open)
-        {
-            this.GetComponent<Rigidbody>().AddForce(new Vector3(10, 0, 10));
-        }
         if (!Open)
         {
-            this.GetComponent<Rigidbody>().AddForce(new Vector3(-10, 0, -10));
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(1, 0, 1));
         }
+        if (Open)
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, -1));
+        }
+    }
+    public void SetOpen()
+    {
+        Open = !Open;
     }
 }
