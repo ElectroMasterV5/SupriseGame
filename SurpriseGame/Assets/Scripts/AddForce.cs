97514c936f159696bb6b061995d647b34675e095
KuroTori <1141759094@qq.com>
2022-10-11 13:58:41 -0400

    SlideDoor

--- SurpriseGame/Assets/Scripts/AddForce.cs
+++ SurpriseGame/Assets/Scripts/AddForce.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AddForce : MonoBehaviour
 {
     public bool Open=false;
+    public bool SlideOpen = false;
     private Ray ray;
     // Start is called before the first frame update
     void Start()
@@ -17,20 +18,26 @@ public class AddForce : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
-            Debug.Log("I pushed!");
+            
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit raycastHit, 10f))
             {
-                Debug.Log("I Hit Something! "+ raycastHit.transform.tag);
+                
                 if (raycastHit.transform.CompareTag("Door"))
                 {
-                    Debug.Log("It's Door!");
+                   
                     raycastHit.transform.GetComponent<AddForce>().SetOpen();
                 }
-                
+                if (raycastHit.transform.CompareTag("SlideDoor"))
+                {
+
+                    raycastHit.transform.GetComponent<AddForce>().SetSlideOpen();
+                }
+
             }
         }
         OpenDoor();
+        OpenSlideDoor();
     }
 
     public void OpenDoor()
@@ -44,8 +51,32 @@ public class AddForce : MonoBehaviour
             this.GetComponent<Rigidbody>().AddForce(new Vector3(-1, 0, -1));
         }
     }
+    public void OpenSlideDoor()
+    {
+        if (!SlideOpen)
+        {
+            if (this.GetComponent<Transform>().position.x > -3.3f)
+            {
+                this.GetComponent<Transform>().Translate(new Vector3(-1, 0, 0) * Time.deltaTime / 3f);
+            }
+            
+        }
+        if (SlideOpen)
+        {
+            if (this.GetComponent<Transform>().position.x < -2f)
+            {
+                this.GetComponent<Transform>().Translate(new Vector3(1, 0, 0) * Time.deltaTime / 3f);
+            }
+                
+        }
+    }
     public void SetOpen()
     {
         Open = !Open;
     }
+    public void SetSlideOpen()
+    {
+        SlideOpen = !SlideOpen;
+    }
+
 }
